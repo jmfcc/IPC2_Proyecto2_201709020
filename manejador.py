854b6a9b965cc93d295d4f144a42fc9a82edb9d7
Jaime Chiroy <jaimefrain08@gmail.com>
2021-03-23 10:57:39 -0600

    Modificación a función generadora de la lista ortogonal (grafo)

--- manejador.py
+++ manejador.py
@@ -1,3 +1,4 @@
+from os import path
 from ClaseLista import ListaImagenes, NodoImagen
 from generarGrafo import grafo
 from LectorXML import leerxml
@@ -13,28 +14,38 @@ def agregaImagen(nombre, filas, columnas, imagen):
     global biblioteca
     biblioteca.agregaImagen(nombre, filas, columnas, imagen)
 
+def listaImagenes():
+    global biblioteca
+    return biblioteca.getNombres()
+
 def cargarArchivo(ruta):
     global biblioteca
     if biblioteca:
         biblioteca = None
         biblioteca = ListaImagenes()
     #Abrir un archvo
-    try:
-        leerxml(ruta, "LECTURA DE ARCHIVO")
-    except:
-        print(" >> Error: Fromato inválido")
-
-def generaImagen():
+    ph, fh = path.split(ruta)
+    nombre, extension = path.splitext(fh)
+    if extension == ".xml":
+        try:
+            leerxml(ruta, nombre+extension)
+            print(" >>> Lectura de archivo completa\n")
+        except:
+            print("\n >>> Error: La lectura no se completo correctamente, verifique su archivo de entrada\n")
+    else:
+        print(" >>> El archivo no es de extensión XML")
+    
+def generaImagen(seleccion, tipo):
     global biblioteca
-    biblioteca.muestraNombres()
-    seleccion = input(" >>> Ingresa el nombre de la matriz para generar su grafo: ")
+    # biblioteca.muestraNombres()
+    # seleccion = input(" >>> Ingresa el nombre de la matriz para generar su grafo: ")
     if seleccion:
         if biblioteca.existeNombre(seleccion):
             nodoMatriz = biblioteca.getNodeByName(seleccion)
-            grafo(nodoMatriz.getNombre(), nodoMatriz.getFilas(), nodoMatriz.getColumnas(), nodoMatriz.getImagen(), "Original")
-            biblioteca.setImgNodeByName(seleccion, "v")
+            grafo(nodoMatriz.getNombre(), nodoMatriz.getFilas(), nodoMatriz.getColumnas(), nodoMatriz.getImagen(), "original")
+            biblioteca.setImgNodeByName(seleccion, tipo)
             nodoMatriz = biblioteca.getNodeByName(seleccion)
-            grafo(nodoMatriz.getNombre(), nodoMatriz.getFilas(), nodoMatriz.getColumnas(), nodoMatriz.getImagen(), "Original")
+            grafo(nodoMatriz.getNombre(), nodoMatriz.getFilas(), nodoMatriz.getColumnas(), nodoMatriz.getImagen(), "modificada")
             #print(nodoMatriz.getMatriz().dameMatrizEnFormato())
         else:
             print(" >>> Selección inválida")
