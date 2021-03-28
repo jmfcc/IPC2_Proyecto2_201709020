5513746b5f5370912a4ab536223be14d7ee35079
Jaime Chiroy <jaimefrain08@gmail.com>
2021-03-27 19:37:40 -0600

    Operaciones Logicas y grafo generado - Terminadas

--- manejador.py
+++ manejador.py
@@ -42,7 +42,113 @@ def union(nombre1, nombre2):
                     aux2 = aux2.getInferior()
                 else:
                     break
-        newImg.muestraLista()
+        imgTemporal = NodoImagen(img1.getNombre()+"_U_"+img2.getNombre(), img1.getFilas(), img1.getColumnas(), newImg)
+        #newImg.muestraLista()
+        generaImagenLogic(imgTemporal)
+    else:
+        pass
+
+def interseccion(nombre1, nombre2):
+    global biblioteca
+    img1 = biblioteca.getNodeByName(nombre1)
+    img2 = biblioteca.getNodeByName(nombre2)
+    if img1.getFilas() == img2.getFilas() and img1.getColumnas() == img2.getColumnas():
+        aux1 = img1.getImagen().getInicio()
+        aux2 = img2.getImagen().getInicio()
+        newImg = imagen()
+        nuevaFila = False
+        while True:
+            if aux1.getCaracter() == "*" and aux2.getCaracter() == "*":
+                newImg.agregaNodo("*",nuevaFila)
+            else:
+                newImg.agregaNodo("-",nuevaFila)
+            nuevaFila = False
+            if aux1.getSiguiente() != None:
+                aux1 = aux1.getSiguiente()
+                aux2 = aux2.getSiguiente()
+            else:
+                nuevaFila = True
+                if aux1.getInferior() != None:
+                    while aux1.getAnterior() != None:
+                        aux1 = aux1.getAnterior()
+                        aux2 = aux2.getAnterior()
+                    aux1 = aux1.getInferior()
+                    aux2 = aux2.getInferior()
+                else:
+                    break
+        imgTemporal = NodoImagen(img1.getNombre()+"_I_"+img2.getNombre(), img1.getFilas(), img1.getColumnas(), newImg)
+        #newImg.muestraLista()
+        generaImagenLogic(imgTemporal)
+    else:
+        pass
+
+def diferencia(nombre1, nombre2):
+    global biblioteca
+    img1 = biblioteca.getNodeByName(nombre1)
+    img2 = biblioteca.getNodeByName(nombre2)
+    if img1.getFilas() == img2.getFilas() and img1.getColumnas() == img2.getColumnas():
+        aux1 = img1.getImagen().getInicio()
+        aux2 = img2.getImagen().getInicio()
+        newImg = imagen()
+        nuevaFila = False
+        while True:
+            if aux1.getCaracter() == "*" and aux2.getCaracter() != "*":
+                newImg.agregaNodo("*",nuevaFila)
+            else:
+                newImg.agregaNodo("-",nuevaFila)
+            nuevaFila = False
+            if aux1.getSiguiente() != None:
+                aux1 = aux1.getSiguiente()
+                aux2 = aux2.getSiguiente()
+            else:
+                nuevaFila = True
+                if aux1.getInferior() != None:
+                    while aux1.getAnterior() != None:
+                        aux1 = aux1.getAnterior()
+                        aux2 = aux2.getAnterior()
+                    aux1 = aux1.getInferior()
+                    aux2 = aux2.getInferior()
+                else:
+                    break
+        imgTemporal = NodoImagen(img1.getNombre()+"_D_"+img2.getNombre(), img1.getFilas(), img1.getColumnas(), newImg)
+        #newImg.muestraLista()
+        generaImagenLogic(imgTemporal)
+    else:
+        pass
+
+def diferenciaSimetrica(nombre1, nombre2):
+    global biblioteca
+    img1 = biblioteca.getNodeByName(nombre1)
+    img2 = biblioteca.getNodeByName(nombre2)
+    if img1.getFilas() == img2.getFilas() and img1.getColumnas() == img2.getColumnas():
+        aux1 = img1.getImagen().getInicio()
+        aux2 = img2.getImagen().getInicio()
+        newImg = imagen()
+        nuevaFila = False
+        while True:
+            if aux1.getCaracter() == "*" and aux2.getCaracter() != "*":
+                newImg.agregaNodo("*",nuevaFila)
+            elif aux2.getCaracter() == "*" and aux1.getCaracter() != "*":
+                newImg.agregaNodo("*",nuevaFila)
+            else:
+                newImg.agregaNodo("-",nuevaFila)
+            nuevaFila = False
+            if aux1.getSiguiente() != None:
+                aux1 = aux1.getSiguiente()
+                aux2 = aux2.getSiguiente()
+            else:
+                nuevaFila = True
+                if aux1.getInferior() != None:
+                    while aux1.getAnterior() != None:
+                        aux1 = aux1.getAnterior()
+                        aux2 = aux2.getAnterior()
+                    aux1 = aux1.getInferior()
+                    aux2 = aux2.getInferior()
+                else:
+                    break
+        imgTemporal = NodoImagen(img1.getNombre()+"_DS_"+img2.getNombre(), img1.getFilas(), img1.getColumnas(), newImg)
+        #newImg.muestraLista()
+        generaImagenLogic(imgTemporal)
     else:
         pass
 
@@ -94,6 +200,9 @@ def generaImagenMod(seleccion, tipo):
             print(" >>> Selección inválida")
     else:
         print(" >>> Aviso: Debes elegir una opción")
+    
+def generaImagenLogic(nodoMatriz):
+    grafo(nodoMatriz.getNombre(), nodoMatriz.getFilas(), nodoMatriz.getColumnas(), nodoMatriz.getImagen(), "opLogic")
 
 def generaImagenO(seleccion):
     global biblioteca
@@ -101,6 +210,17 @@ def generaImagenO(seleccion):
         if biblioteca.existeNombre(seleccion):
             nodoMatriz = biblioteca.getNodeByName(seleccion)
             grafo(nodoMatriz.getNombre(), nodoMatriz.getFilas(), nodoMatriz.getColumnas(), nodoMatriz.getImagen(), "original")
+        else:
+            print(" >>> Selección inválida")
+    else:
+        print(" >>> Aviso: Debes elegir una opción")
+
+def generaImagenO2(seleccion):
+    global biblioteca
+    if seleccion:
+        if biblioteca.existeNombre(seleccion):
+            nodoMatriz = biblioteca.getNodeByName(seleccion)
+            grafo(nodoMatriz.getNombre(), nodoMatriz.getFilas(), nodoMatriz.getColumnas(), nodoMatriz.getImagen(), "original2")
         else:
             print(" >>> Selección inválida")
     else:
